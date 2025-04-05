70d38bdf420cc6e1264e7a1ac0840bed79932c29
Developer <Lepiozdyx@gmail.com>
2025-04-05 10:18:39 -0300

    Initial Commit

---
+++ Portsmouth/PortsmouthApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortsmouthApp.swift
+//  Portsmouth
+//
+//  Created by Alex on 05.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct PortsmouthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
